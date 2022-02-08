cafa1ea4f084abc044fe56e466d87059f72fd4cc
Bas Bruin <487790@student.fontys.nl>
2022-02-08 14:45:58 +0100

    Add project files.

---
+++ Testhash/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Testhash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbxToBeHashed = new System.Windows.Forms.RichTextBox();
+            this.rtbxHasedText = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbxToBeHashed
+            // 
+            this.rtbxToBeHashed.Location = new System.Drawing.Point(187, 155);
+            this.rtbxToBeHashed.Name = "rtbxToBeHashed";
+            this.rtbxToBeHashed.Size = new System.Drawing.Size(100, 96);
+            this.rtbxToBeHashed.TabIndex = 0;
+            this.rtbxToBeHashed.Text = "";
+            // 
+            // rtbxHasedText
+            // 
+            this.rtbxHasedText.Location = new System.Drawing.Point(401, 155);
+            this.rtbxHasedText.Name = "rtbxHasedText";
+            this.rtbxHasedText.Size = new System.Drawing.Size(100, 96);
+            this.rtbxHasedText.TabIndex = 1;
+            this.rtbxHasedText.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbxHasedText);
+            this.Controls.Add(this.rtbxToBeHashed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbxToBeHashed;
+        private System.Windows.Forms.RichTextBox rtbxHasedText;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Testhash/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Testhash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(rtbxToBeHashed.Text);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+                rtbxHasedText.Text = sb.ToString();
+            }
+        }
+    }
+}
